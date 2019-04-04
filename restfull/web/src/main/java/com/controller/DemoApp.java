1971467ac09ace7f4e20fe08de1ac5e04399fa27
danielshsp <danielshjobs@gmail.com>
2019-04-04 15:26:05 +0300

    sts pro rest multi module

---
+++ restfull/web/src/main/java/com/controller/DemoApp.java
@@ -0,0 +1,16 @@
+package com.controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@SpringBootApplication
+@EntityScan(basePackages = {"com.domain"})
+public class DemoApp {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(DemoApp.class, args);
+
+	}
+
+}
